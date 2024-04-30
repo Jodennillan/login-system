1989c6bb3a8561c5d229b06fadd0bd968737d929
Jodennillan <161803001+Jodennillan@users.noreply.github.com>
2024-04-30 15:33:04 +0300

    login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Online University Application</title>
+    <title>Login system </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <style>
         body {
